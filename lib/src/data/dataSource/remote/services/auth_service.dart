ff83f790634ad426e7e91a6981251d646754969f
Ahtnezev <41706384+Ahtnezev@users.noreply.github.com>
2025-08-31 00:34:22 -0600

    [update] data model

--- lib/src/data/dataSource/remote/services/auth_service.dart
+++ lib/src/data/dataSource/remote/services/auth_service.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:indriver_clone_flutter/src/data/api/api_config.dart';
+import 'package:indriver_clone_flutter/src/domain/models/auth_response.dart';
 
 class AuthService {
 
-  Future<dynamic> login(String email, String password) async {
+  Future<AuthResponse?> login(String email, String password) async {
     try {
       Uri url = Uri.http(ApiConfig.API_PROJECT, '/auth/login');
       // print("URL: $url");
@@ -15,11 +16,16 @@ class AuthService {
       });
       final response = await http.post(url, headers: headers, body: body);
       final data = json.decode(response.body);
-      print("Data Remote: $data");
+      AuthResponse authResponse = AuthResponse.fromJson(data);
 
-      return data;
+      print("Data Remote: ${authResponse.toJson()}");
+      print("Token: ${authResponse.token}");
+
+
+      return authResponse;
     } catch(e) {
       print("Error: $e");
+      return null;
     }
   }
 
